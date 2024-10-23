47b33ad13cec5fd60f7211810c348aeed8a80dbb
Haneef Ghanim <haneef.ghanim@gmail.com>
2024-10-22 23:05:01 -0400

    Got the app working.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import Image from 'next/image';
 import localFont from 'next/font/local';
+import { useState } from 'react';
+import { Button } from '@/components/ui/Button';
 
 const geistSans = localFont({
     src: './fonts/GeistVF.woff',
@@ -12,98 +13,35 @@ const geistMono = localFont({
     weight: '100 900'
 });
 
+const roots = ['ك ت ب', 'ع ل م', 'د ر س', 'ذ ه ب', 'ن ز ل', 'ف ت ح', 'ج ل س', 'خ ر ج'];
+
 export default function Home() {
+    const [pattern, setPattern] = useState('');
+    const [root, setRoot] = useState('');
+    const onClickGenerate = function () {
+        setPattern((Math.floor(Math.random() * 10) + 1).toString());
+        setRoot(roots[Math.floor(Math.random() * 8)]);
+    };
+
     return (
         <div
             className={`${geistSans.variable} ${geistMono.variable} grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20`}
         >
-            <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-                <Image
-                    className="dark:invert"
-                    src="https://nextjs.org/icons/next.svg"
-                    alt="Next.js logo"
-                    width={180}
-                    height={38}
-                    priority
-                />
-                <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
-                    <li className="mb-2">
-                        Get started by editing{' '}
-                        <code className="rounded bg-black/[.05] px-1 py-0.5 font-semibold dark:bg-white/[.06]">
-                            pages/index.tsx
-                        </code>
-                        .
-                    </li>
-                    <li>Save and see your changes instantly.</li>
-                </ol>
-
-                <div className="flex flex-col items-center gap-4 sm:flex-row">
-                    <a
-                        className="bg-foreground text-background flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm transition-colors hover:bg-[#383838] sm:h-12 sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            className="dark:invert"
-                            src="https://nextjs.org/icons/vercel.svg"
-                            alt="Vercel logomark"
-                            width={20}
-                            height={20}
-                        />
-                        Deploy now
-                    </a>
-                    <a
-                        className="flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Read our docs
-                    </a>
-                </div>
+            <main className="row-start-2 flex flex-col items-center gap-2 text-center sm:items-start sm:text-left">
+                <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight">Sarfing</h1>
+                <p className="mb-2 max-w-[300px] sm:max-w-full">
+                    Click the button below to get a 3-letter root and a pattern to conjugate it in.
+                </p>
+                <Button size="lg" onClick={onClickGenerate}>
+                    {pattern && root ? 'Generate again' : 'Generate'}
+                </Button>
+                {pattern && root && (
+                    <div className="mt-2 rounded bg-blue-100 px-[0.5rem] py-[0.2rem] text-3xl">
+                        Do &quot;<strong className="font-medium">{root}</strong>&quot; in pattern{' '}
+                        <strong className="font-medium">{pattern}</strong>.
+                    </div>
+                )}
             </main>
-            <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image aria-hidden src="https://nextjs.org/icons/file.svg" alt="File icon" width={16} height={16} />
-                    Learn
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="https://nextjs.org/icons/window.svg"
-                        alt="Window icon"
-                        width={16}
-                        height={16}
-                    />
-                    Examples
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="https://nextjs.org/icons/globe.svg"
-                        alt="Globe icon"
-                        width={16}
-                        height={16}
-                    />
-                    Go to nextjs.org →
-                </a>
-            </footer>
         </div>
     );
 }
